8a7189b55114f95b4ab34112efa1adb6c64bcd35
Monu Gupta <monug1513@gmail.com>
2022-09-26 09:24:25 +0530

    initial commit

---
+++ js.js
@@ -0,0 +1,39 @@
+// For button one
+
+let btn=document.getElementById("btn1")
+btn.addEventListener("click",myfunction)
+function myfunction()
+{
+
+document.getElementById("one").src="image/download (1).jpg"
+
+}
+
+
+//For button two
+let btn2=document.getElementById("btn2")
+btn2.addEventListener("click",mysecondfunction)
+function mysecondfunction()
+{
+
+document.getElementById("one").src="image/download.jpg"
+
+}
+//For button three
+let btn3=document.getElementById("btn3")
+btn3.addEventListener("click",mythirdfunction)
+function mythirdfunction()
+{
+
+document.getElementById("one").src="image/3.jpg"
+
+}
+//For button fouth
+let btn4=document.getElementById("btn4")
+btn4.addEventListener("click",myfourthfunction)
+function myfourthfunction()
+{
+
+document.getElementById("one").src="image/4.jpg"
+
+}
